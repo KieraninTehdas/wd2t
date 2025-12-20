2a11f61287187a0683b4b1a5cb014c130036d63b
Kieran Porter <kieranintehdas@gmail.com>
2025-12-20 00:36:24 +0000

    Start on user sign up flow

---
+++ app/controllers/sign_ups_controller.rb
@@ -0,0 +1,26 @@
+class SignUpsController < ApplicationController
+  allow_unauthenticated_access
+
+  def show
+    @user = User.new
+  end
+
+  def create
+    puts sign_up_params
+    @user = User.new(sign_up_params)
+    if @user.save
+      start_new_session_for(@user)
+      redirect_to root_path
+    else
+      render :show, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def sign_up_params
+    sign_up_params = params.expect(user: %i[first_name last_name email_address password password_confirmation])
+    sign_up_params[:display_name] = "#{sign_up_params[:first_name]} #{sign_up_params[:last_name]}"
+    sign_up_params
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   root to: "decisions#index"
   resource :session
+  resource :sign_up
   resources :passwords, param: :token
   resources :decisions
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
