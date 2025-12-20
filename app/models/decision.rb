a0009b8c5a89058f7a97b5735558b65b5889e40c
Kieran Porter <kieranintehdas@gmail.com>
2025-12-20 23:02:47 +0000

    Default decision status to pending and add a select to the new decision form

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -3,6 +3,8 @@
 class Decision < ApplicationRecord
   belongs_to :creator, class_name: "User", default: -> { Current.user }
 
+  attribute :status, default: -> { :pending }
+
   validates :title, presence: true
 
   enum :status, {
